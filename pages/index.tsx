e3ab4f1488265e0fede57ee770248baba54be478
bruce <cnhxunoo@gmail.com>
2023-02-13 16:36:27 +0800

    change index desc

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,22 +21,22 @@ const Home: NextPage = () => {
         你好， 我主要从事软件开发和技术培训。
       </p>
       <h3 className='md:text-xl m-4 text-left'>
-        Bruce -- (Python开发培训 / 算法培训 / 机器学习 / Qt开发 / C++开发 /
+        Bruce -- (Python开发 / Java Kotlin / 视觉应用 / Qt开发 / C++开发 /
         通信开发 / APP开发)
       </h3>
       <Heading title={'作品'} />
       <p className='m-4'>
-        Bruce是一个软件开发人员，主要从事Web服务开发，工业软件开发，通信软件开发，等等,
-        python培训，数据分析，算法设计培训, 包括Numpy, Pandas, Scipy,
-        Scikit-Learn, Torch, 机器学习模型设计，
-        算法设计，深度学习模型设计以及服务部署。
+        Bruce是一个软件开发人员，主要从事应用软件开发，工业软件开发，通信软件开发，web前后端开发等等,
+        有长期使用qt, pyqt, java从事桌面应用开发经验，
+        iot系统开发经验，工业相机接口开发经验等等。
       </p>
       <ButtonLink link={'/work'} title={'作品'} />
       <Heading title={'内容'} />
       <div className='m-4 ml-5'>
         <p>
           <strong className='pr-10'>培训:</strong>
-          Python基础，数据分析，数据挖掘，特征提取, 机器学习，深度学习
+          使用Python以及Numpy, Scipy, Pandas,
+          Scikit-learn，pytorch做数据分析，数据建模，机器学习，深度学习进行培训。
         </p>
         <p>
           <strong className='pr-3'>桌面开发:</strong>工业软件开发, 通信软件开发,
