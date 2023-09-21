b510d87aafde6bc41fca96c223a821db363e2f5f
bruce <cnhxunoo@gmail.com>
2023-09-21 20:03:24 +0800

    change code style

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -9,7 +9,7 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { docco } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
+import { materialDark as style } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 interface Params extends ParsedUrlQuery {
 	slug: string
@@ -40,7 +40,7 @@ const SinglePost: NextPage<SinglePost> = ({
 							return !inline && match ? (
 								<SyntaxHighlighter
 									{...props}
-									style={docco}
+									style={style}
 									language={match[1]}
 									PreTag='div'
 								>
--- pages/work/[slug].tsx
+++ pages/work/[slug].tsx
@@ -10,7 +10,7 @@ import remarkGfm from 'remark-gfm'
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 
-import { docco } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
+import { materialDark as style } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 interface Params extends ParsedUrlQuery {
 	slug: string
@@ -41,7 +41,7 @@ const SingleWork: NextPage<SinglePost> = ({
 							return !inline && match ? (
 								<SyntaxHighlighter
 									{...props}
-									style={docco}
+									style={style}
 									language={match[1]}
 									PreTag='div'
 								>
