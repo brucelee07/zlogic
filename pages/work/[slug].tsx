30c80800897d6e43b8ae4bca3e128458840d51ae
bruce <cnhxunoo@gmail.com>
2023-09-21 19:34:11 +0800

    remove children as props

--- pages/work/[slug].tsx
+++ pages/work/[slug].tsx
@@ -12,82 +12,84 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { dark } from 'react-syntax-highlighter/dist/cjs/styles/prism/solarized-dark-atom'
 
 interface Params extends ParsedUrlQuery {
-  slug: string
+	slug: string
 }
 
 type SinglePost = {
-  frontMatter: {
-    title: string
-    date: string
-  }
-  mdxSource: string
+	frontMatter: {
+		title: string
+		date: string
+	}
+	mdxSource: string
 }
 
 const SingleWork: NextPage<SinglePost> = ({
-  frontMatter: { title, date },
-  mdxSource,
+	frontMatter: { title, date },
+	mdxSource,
 }: SinglePost) => {
-  return (
-    <>
-      <div className='min-h-[88vh] text-gray-700 p-5'>
-        <h3 className='mt-3 text-center font-bold text-2xl'>{title}</h3>
-        <p className='text-sm text-right mb-2 pr-2'>{date}</p>
-        <ReactMarkdown
-          children={mdxSource}
-          remarkPlugins={[remarkGfm]}
-          components={{
-            code({ node, inline, className, children, ...props }) {
-              const match = /language-(\w+)/.exec(className || '')
-              return !inline && match ? (
-                <SyntaxHighlighter
-                  {...props}
-                  children={String(children).replace(/\n$/, '')}
-                  style={dark}
-                  language={match[1]}
-                  PreTag='div'
-                />
-              ) : (
-                <code {...props} className={className}>
-                  {children}
-                </code>
-              )
-            },
-          }}
-        />
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className='min-h-[88vh] text-gray-700 p-5'>
+				<h3 className='mt-3 text-center font-bold text-2xl'>{title}</h3>
+				<p className='text-sm text-right mb-2 pr-2'>{date}</p>
+				<ReactMarkdown
+					remarkPlugins={[remarkGfm]}
+					components={{
+						code({ node, inline, className, children, ...props }) {
+							const match = /language-(\w+)/.exec(className || '')
+							return !inline && match ? (
+								<SyntaxHighlighter
+									{...props}
+									style={dark}
+									language={match[1]}
+									PreTag='div'
+								>
+									{String(children).replace(/\n$/, '')}
+								</SyntaxHighlighter>
+							) : (
+								<code {...props} className={className}>
+									{children}
+								</code>
+							)
+						},
+					}}
+				>
+					{mdxSource}
+				</ReactMarkdown>
+			</div>
+		</>
+	)
 }
 export const getStaticProps: GetStaticProps = async (context) => {
-  const { slug } = context.params as Params
-  const markdownwithMeta = fs.readFileSync(
-    path.join('works', slug + '.md'),
-    'utf-8',
-  )
-  const { data: frontMatter, content } = matter(markdownwithMeta)
-  const mdxSource = content
-  return {
-    props: {
-      frontMatter,
-      slug,
-      mdxSource,
-    },
-  }
+	const { slug } = context.params as Params
+	const markdownwithMeta = fs.readFileSync(
+		path.join('works', slug + '.md'),
+		'utf-8',
+	)
+	const { data: frontMatter, content } = matter(markdownwithMeta)
+	const mdxSource = content
+	return {
+		props: {
+			frontMatter,
+			slug,
+			mdxSource,
+		},
+	}
 }
 
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
-  const files = fs.readdirSync(path.join('works'))
-  const paths = files.map((file) => {
-    return {
-      params: {
-        slug: file.split('.')[0],
-      },
-    }
-  })
-  return {
-    paths,
-    fallback: false,
-  }
+	const files = fs.readdirSync(path.join('works'))
+	const paths = files.map((file) => {
+		return {
+			params: {
+				slug: file.split('.')[0],
+			},
+		}
+	})
+	return {
+		paths,
+		fallback: false,
+	}
 }
 
 export default SingleWork
